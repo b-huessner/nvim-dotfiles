f48d37a0d1b1a6af8c5ea1dc05a847726837d8f3
Bastian Hüßner <bhuessner@exagon.de>
2024-01-17 08:02:09 +0100

    Add inlay hints for volar (vue)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,30 @@ return {
             },
           },
         },
+        volar = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                enumMemberValues = {
+                  enabled = true,
+                },
+                functionLikeReturnTypes = {
+                  enabled = true,
+                },
+                propertyDeclarationTypes = {
+                  enabled = true,
+                },
+                parameterTypes = {
+                  enabled = true,
+                  suppressWhenArgumentMatchesName = true,
+                },
+                variableTypes = {
+                  enabled = true,
+                },
+              },
+            },
+          },
+        },
         tsserver = {
           settings = {
             typescript = {
