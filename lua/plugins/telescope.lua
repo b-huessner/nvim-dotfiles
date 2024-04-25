12a3e6dc2843db209bb150ebc6f17ecdaaa1bcfd
Bastian Hüßner <bastian@bastianhuessner.de>
2024-04-25 08:45:14 +0200

    Add snippet and telescope config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+        "--glob",
+        "!{**/.git/*,**/node_modules/*,**/pkg/mod/*,**/yarn.lock,**/.local/*}",
+      },
+    },
+  },
+}
